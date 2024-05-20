dcc881471afaa10dbca9bf00a2e25a7dccb05e30
azmerhos <azmerhos@gmail.com>
2024-05-20 02:52:47 +0200

    short optimization and comment edit under makoto-sama guidances

--- commands/interaction/weather.js
+++ commands/interaction/weather.js
@@ -19,67 +19,47 @@ module.exports = {
         )
     ),
   async execute(interaction) {
+
+    //Declare functions
+    const cityget = async function (request) {
+        //async because we need to wait fetch result or else pos will be undefined
+        let pos = new Map(); // Map = key => value array
+        await fetch(request)
+          .then((response) => {
+            // response = result of request
+            if (!response.ok) { return console.error(`HTTP status was not OK. ${response.status}`);} // not Status 200 
+            return response.json();
+          })
+          .then((data) => { pos.set("lat", data[0].lat).set("lon", data[0].lon); }); // data = response.json()
+        return pos;
+      };
+      
+      const weatherget = async function (pos) {
+        let values = new Map();
+        const weatherRequest = new Request( `https://api.openweathermap.org/data/2.5/weather?lat=${pos.get("lat")}&lon=${pos.get("lon")}&appid=${OWtoken}&units=metric` );
+        await fetch(weatherRequest)
+          .then((response) => {
+            if (!response.ok) { return console.error(`HTTP status was not OK. ${response.status}`); }
+            return response.json();
+          })
+          .then((data) => {
+            values
+              .set("city", data.name)
+              .set("weather", data.weather[0].main)
+              .set("temp", data.main.temp)
+              .set("hum", data.main.humidity);
+          });
+        return values;
+      };
+      
+    // Command begin
     const name = interaction.options.getString("name");
-    const zipcode = interaction.options.getInteger("zipcode");
-    const countryCode = interaction.options.getString("country_code");
     let data, pos, request;
-    if (!name) {
-        await interaction.reply({
-          content: `Incorrect input. Is your input correct ?`,
-          ephemeral: true,
-        });
-        return;
-      } 
-     else {
-      request = new Request(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${name}&appid=${OWtoken}`
-      );
-    }
+    if (!name) { return interaction.reply({ content: `Did you forgot the city ? Try again.`, ephemeral: true, }); } 
+    request = new Request(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&appid=${OWtoken}`);
     pos = await cityget(request);
     data = await weatherget(pos);
-    await interaction.reply(
-      `Weather at ${data.get("city")} : ${data.get("weather")}, Temperature : ${data.get("temp")}°C, Humidity: ${data.get("hum")}%`
-    );
+    await interaction.reply( `Weather at ${data.get("city")} : ${data.get("weather")}, Temperature : ${data.get("temp")}°C, Humidity: ${data.get("hum")}%` );
   },
 };
 
-const cityget = async function (request) {
-  //async because we need to wait fetch result or else pos will be undefined
-  let pos = new Map(); // Map = key => value array
-  await fetch(request)
-    .then((response) => {
-      // response = result of request
-      if (!response.ok) {
-        // not Status 200 ?
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // data = response.json()
-      pos.set("lat", data[0].lat).set("lon", data[0].lon);
-    });
-  return pos;
-};
-
-const weatherget = async function (pos) {
-  let values = new Map();
-  const weatherRequest = new Request(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${pos.get("lat")}&lon=${pos.get("lon")}&appid=${OWtoken}&units=metric`
-  );
-  await fetch(weatherRequest)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      values
-        .set("city", data.name)
-        .set("weather", data.weather[0].main)
-        .set("temp", data.main.temp)
-        .set("hum", data.main.humidity);
-    });
-  return values;
-};
